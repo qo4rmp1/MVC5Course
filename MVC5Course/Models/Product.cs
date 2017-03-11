954c210c796a434b6ceda205ade97df4268a710e
qo4rmp1 <qo4rmp1@gmail.com>
2017-03-11 14:51:53 +0800

    05 練習表單欄位驗證，套用 Required 與 Range 驗證屬性，並自訂錯誤訊息

--- MVC5Course/Models/Product.cs
+++ MVC5Course/Models/Product.cs
@@ -9,24 +9,30 @@
 
 namespace MVC5Course.Models
 {
-    using System;
-    using System.Collections.Generic;
-    
-    public partial class Product
+  using System;
+  using System.Collections.Generic;
+  using System.ComponentModel.DataAnnotations;
+
+  public partial class Product
+  {
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+    public Product()
     {
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
-        public Product()
-        {
-            this.OrderLine = new HashSet<OrderLine>();
-        }
-    
-        public int ProductId { get; set; }
-        public string ProductName { get; set; }
-        public Nullable<decimal> Price { get; set; }
-        public Nullable<bool> Active { get; set; }
-        public Nullable<decimal> Stock { get; set; }
-    
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
-        public virtual ICollection<OrderLine> OrderLine { get; set; }
+      this.OrderLine = new HashSet<OrderLine>();
     }
+
+    public int ProductId { get; set; }
+    //可參考PDF(P62)
+    [Required(ErrorMessage ="請輸入商品名稱{0}")]
+    public string ProductName { get; set; }
+    [Required][Range(100, 500,ErrorMessage ="價錢需輸入100~500,請重新輸入{0}")]
+    public Nullable<decimal> Price { get; set; }
+    [Required]
+    public Nullable<bool> Active { get; set; }
+    [Required]
+    public Nullable<decimal> Stock { get; set; }
+
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+    public virtual ICollection<OrderLine> OrderLine { get; set; }
+  }
 }
