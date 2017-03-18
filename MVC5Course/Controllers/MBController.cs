142c8ae0e92c10f9bdc525234c582bd3cce88d0e
qo4rmp1 <qo4rmp1@gmail.com>
2017-03-18 16:26:11 +0800

    示範 FormCollection 用法

---
+++ MVC5Course/Controllers/MBController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Course.Controllers
+{
+    public class MBController : Controller
+    {
+        // GET: MB
+        public ActionResult Index()
+        {
+            return View();
+        }
+        /*
+         新增檢視
+         選擇Create->LoginVM Model->空白
+         */
+        public ActionResult Form1()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Form1(FormCollection form)
+        {
+            return Content(form["UserName"]);
+            /*
+             這兩者的值都一樣,但不建議使用Request.Form
+             return Content(Request.Form["UserName"]);
+             return Content(form["UserName"]);
+             */
+        }
+    }
+}
