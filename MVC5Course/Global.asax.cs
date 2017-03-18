1b5ef7547178cc579b80298dbe7562e2d96eaa9e
qo4rmp1 <qo4rmp1@gmail.com>
2017-03-18 13:38:45 +0800

    21 練習將 WebFormViewEngine 從 ViewEngines.Engines 中移除

--- MVC5Course/Global.asax.cs
+++ MVC5Course/Global.asax.cs
@@ -18,6 +18,12 @@ namespace MVC5Course
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            //因為這只要註冊一次, 統一寫在Global.asax.cs
+            //清除所有引擎(WebForm引擎、Razor引擎)
+            ViewEngines.Engines.Clear();
+            //加入Razor引擎
+            ViewEngines.Engines.Add(new RazorViewEngine());
         }
     }
 }
